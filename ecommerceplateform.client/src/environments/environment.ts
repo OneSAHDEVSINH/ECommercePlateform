97c8464937042819c8273ccee5e413c603b79f74
SAHDEVSINH PARMAR <sahdevsinhparmar1753@gmail.com>
2025-05-08 23:32:27 +0530

    Update API URL in environment files
    Replaced the old `apiUrl` values in `environment.prod.ts` and `environment.ts` with a new endpoint: `https://xctnhw3f-44362.inc1.devtunnels.ms/api`. This change ensures the application points to the correct API for both production and development environments.

--- ecommerceplateform.client/src/environments/environment.ts
+++ ecommerceplateform.client/src/environments/environment.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
   //apiUrl: 'https://localhost:44362/api'
-  apiUrl: 'https://localhost:7059/api'
+  //apiUrl: 'https://localhost:7059/api'
+  apiUrl: 'https://xctnhw3f-44362.inc1.devtunnels.ms/api' //home dev tunnel
 
 }; 
