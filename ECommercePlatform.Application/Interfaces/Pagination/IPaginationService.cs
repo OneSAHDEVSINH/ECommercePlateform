274b9f4f14adef2b0007be8a7c7471968434025e
OneSAHDEVSINH <sahdevsinhparmar1753@gmail.com>
2025-06-03 17:51:13 +0530

    Cleanup: Remove commented-out pagination code
    Removed unnecessary commented-out methods in `IPaginationService.cs` and `PaginationService.cs`. The `CreatePagedResponseAsync` method is now the only active implementation, streamlining the codebase for better readability and maintainability.

--- ECommercePlatform.Application/Interfaces/Pagination/IPaginationService.cs
+++ ECommercePlatform.Application/Interfaces/Pagination/IPaginationService.cs
@@ -5,39 +5,6 @@ namespace ECommercePlatform.Application.Interfaces.Pagination
 {
     public interface IPaginationService
     {
-        //Task<PagedResponse<TDto>> CreatePagedResponseAsync<TEntity, TDto>(
-        //    IQueryable<TEntity> query,
-        //    PagedRequest request,
-        //    Func<TEntity, TDto> mapFunction,
-        //    CancellationToken cancellationToken = default)
-        //    where TEntity : class;
-
-        //IQueryable<T> ApplyPaging<T>(
-        //    IQueryable<T> query,
-        //    int pageNumber,
-        //    int pageSize);
-
-        //IQueryable<T> ApplySearch<T>(
-        //    IQueryable<T> query,
-        //    string? searchText,
-        //    params Expression<Func<T, bool>>[] searchPredicates);
-
-        //IQueryable<T> ApplySort<T>(
-        //    IQueryable<T> query,
-        //    string? sortColumn,
-        //    string? sortDirection,
-        //    Dictionary<string, Expression<Func<T, object>>>? sortExpressions = null);
-
-        ////alone method for creating paged result with filter expression and mapping function
-        //Task<PagedResponse<TResult>> CreatePagedResultAsync<TEntity, TResult>(
-        //    IQueryable<TEntity> query,
-        //    PagedRequest request,
-        //    Expression<Func<TEntity, bool>>? filterExpression = null,
-        //    Func<TEntity, TResult> mappingFunction = null!)
-        //    where TEntity : class
-        //    where TResult : class;
-
-
         Task<PagedResponse<TDto>> CreatePagedResponseAsync<TEntity, TDto>(
             IQueryable<TEntity> query,
             PagedRequest request,
